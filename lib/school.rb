9a7f21a6f788c605fa98795c25f6163e73388beb
Matt Mason <masonmj10@gmail.com>
2020-11-30 13:49:44 -0500

    commit tree a3202e4d1851a2569bfd218ab0c3b4763aeb5135

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+
+class School
+
+attr_accessor :name, :roster, :student_name, :grade
+
+def initialize(name)
+    @name = name 
+    @roster = {}
+
+end
+
+def add_student(student_name, grade)
+    if @roster[grade]
+        @roster[grade] << student_name
+    else 
+        @roster[grade] = []
+        @roster[grade] << student_name
+    end
+  
+end 
+
+def grade(grade)
+    roster[grade]
+end
+
+def sort
+    roster.each do |name, grade|
+        grade.sort!
+    end
+end 
+end
